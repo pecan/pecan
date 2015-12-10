35c8f897b29e7638ca367786634969c8c490819e
Alfredo Deza <alfredo@deza.pe>
2015-12-10 11:22:52 -0500

    use the new utility helper in Pecan's own unit tests

--- pecan/tests/__init__.py
+++ pecan/tests/__init__.py
@@ -4,17 +4,10 @@ if sys.version_info < (2, 7):
     from unittest2 import TestCase  # pragma: nocover
 else:
     from unittest import TestCase  # pragma: nocover
+from pecan.testing import reset_global_config
 
 
 class PecanTestCase(TestCase):
 
     def setUp(self):
-        self.addCleanup(self._reset_global_config)
-
-    def _reset_global_config(self):
-        from pecan import configuration
-        configuration.set_config(
-            dict(configuration.initconf()),
-            overwrite=True
-        )
-        os.environ.pop('PECAN_CONFIG', None)
+        self.addCleanup(reset_global_config)
