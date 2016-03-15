f8ced0b7c6bdb98f1f697886542be7b4f3b94986
Ryan Petrello <lists@ryanpetrello.com>
2016-03-15 10:08:47 -0400

    Fix a bug in controller argspec detection when class-based decorators are used.

--- pecan/tests/test_util.py
+++ pecan/tests/test_util.py
@@ -113,3 +113,26 @@ class TestArgSpec(unittest.TestCase):
 
         argspec = util._cfg(RootController.index)['argspec']
         assert argspec.args == ['self', 'a', 'b', 'c']
+
+    def test_class_based_decorator(self):
+
+        class deco(object):
+
+            def __init__(self, arg):
+                self.arg = arg
+
+            def __call__(self, f):
+                @functools.wraps(f)
+                def wrapper(*args, **kw):
+                    assert self.arg == '12345'
+                    return f(*args, **kw)
+                return wrapper
+
+        class RootController(object):
+            @expose()
+            @deco('12345')
+            def index(self, a, b, c):
+                return 'Hello, World!'
+
+        argspec = util._cfg(RootController.index)['argspec']
+        assert argspec.args == ['self', 'a', 'b', 'c']
