a5a4c81c5c23551c9b3d127f11025dbce107c1d1
Ryan Petrello <lists@ryanpetrello.com>
2016-06-02 16:02:14 -0400

    Officially support Python 3.5.

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Topic :: Software Development :: Libraries :: Application Frameworks'
